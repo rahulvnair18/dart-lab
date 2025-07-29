3e39ce32e01dc0e0343ea4ee36ad960d5b376ad8
Mr-Leftiee <mr.leftie18@gmail.com>
2025-07-29 10:53:40 +0530

    first 2 programs

---
+++ p1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Rahul";
+  int age = 22;
+  double height = 6.3;
+  bool isStudent = true;
+  print(
+    "$name is $age years old and his height is $height and he is a $isStudent student",
+  );
+}
